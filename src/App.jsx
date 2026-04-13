ce3bb49434d2639d185cfe68bb874ddb3b25af5e
Franco Schmidt <francojschmidt@gmail.com>
2026-04-13 15:52:31 -0500

    started map stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,22 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import washuLogo from './assets/washu.svg'
+import { MapContainer, Popup, TileLayer, useMap } from 'react-leaflet';
 import './App.css'
 
 function App() {
 
+
+
   return (
     <>
-      <div>
-        <a href="https://washu.edu" target="_blank" rel="noreferrer">
-          <img src={washuLogo} className="logo washu" alt="WashU logo" />
-        </a>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>WashU + Vite + React</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the WashU, Vite and React logos to learn more
-      </p>
+      <header>
+      </header>
+
+      <main>
+        <div id="map">
+        </div>
+      </main>
+
+      <footer>
+      </footer>
     </>
   )
 }
