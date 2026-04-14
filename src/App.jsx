f4cf7bf7b941af5c9ec066ac8504bd7242c7a1fe
francojschmidt <francojschmidt@gmail.com>
2026-04-13 19:15:05 -0500

    basic map, removed uneeded assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { MapContainer, Popup, TileLayer, useMap } from 'react-leaflet';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import './App.css'
 
+const stlPosition = [38.6274, -90.1982]
+
 function App() {
-
-
 
     return (
         <>
@@ -11,14 +11,25 @@ function App() {
             </header>
 
             <main>
-                <div id="map">
+                <div>
+                    <MapContainer id="map" center={stlPosition} zoom={10}>
+                        <TileLayer
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                        />
+                        <Marker position={stlPosition}>
+                            <Popup>
+                                St. Louis, MO
+                            </Popup>
+                        </Marker>
+                    </MapContainer>
                 </div>
             </main>
 
             <footer>
             </footer>
         </>
-    )
+    );
 }
 
 export default App
