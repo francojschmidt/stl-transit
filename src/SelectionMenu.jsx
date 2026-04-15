b53d22d395377ff654edf6109cfae00519f5a727
Franco Schmidt <francojschmidt@gmail.com>
2026-04-15 14:19:36 -0500

    seletcionmenu file

---
+++ src/SelectionMenu.jsx
@@ -0,0 +1,16 @@
+import Route from './Route.jsx'
+
+function SelectionMenu() {
+    return (
+        <form>
+            <div id="linkMenu">
+            </div>
+            <div id="busMenu">
+            </div>
+            <div id="vehicleMenu">
+            </div>
+        </form>
+    )
+}
+
+export default SelectionMenu
