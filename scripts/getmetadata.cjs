6018d6b853a491a6cdc3d29df45ee45467e4623e
francojschmidt <francojschmidt@gmail.com>
2026-05-05 23:54:30 -0500

    route metadata for selection menu

---
+++ scripts/getmetadata.cjs
@@ -0,0 +1,67 @@
+const fs = require("fs");
+const path = require("path");
+
+const inputFile = path.join("../data/google_transit/", "routes.txt");
+const outputFile = path.join("./src/", "routesMetadata.js");
+
+// might add the real ones later idk, would have to do it by hand :(
+const colors = [
+  "#1E88E5",
+  "#43A047",
+  "#E53935",
+  "#FB8C00",
+  "#8E24AA",
+  "#00ACC1",
+  "#FDD835",
+  "#6D4C41",
+  "#3949AB",
+  "#00897B"
+];
+
+function parseCSV(text) {
+  const lines = text.trim().split("\n");
+  const headers = lines[0].split(",");
+
+  return lines.slice(1).map((line) => {
+    const values = line.split(",");
+    const row = {};
+
+    headers.forEach((header, index) => {
+      row[header.trim()] = values[index]?.trim() || "";
+    });
+
+    return row;
+  });
+}
+
+function generateMetadata() {
+  const fileContent = fs.readFileSync(inputFile, "utf8");
+  const routes = parseCSV(fileContent);
+
+  const metadata = routes.map((route, index) => {
+    const routeId = route.route_id;
+    const shortName = route.route_short_name || routeId;
+    const longName = route.route_long_name || shortName;
+
+    return {
+      id: routeId,
+      name: `${shortName} - ${longName}`,
+      color: colors[index % colors.length],
+      files: [
+        `/src/assets/routes/70006_${routeId}_0.geojson`,
+        `/src/assets/routes/70006_${routeId}_1.geojson`
+      ]
+    };
+  });
+
+  const output = `const routesMetadata = ${JSON.stringify(metadata, null, 2)};
+
+export default routesMetadata;
+`;
+
+  fs.writeFileSync(outputFile, output);
+
+  console.log(`Created routesMetadata.js with ${metadata.length} routes`);
+}
+
+generateMetadata();
---
+++ src/routesMetadata.js
@@ -0,0 +1,559 @@
+// only one direction for 19595, 19609, 19639, 19640
+const routesMetadata = [
+  {
+    "id": "19251B",
+    "name": "MLB - MetroLink Blue Line",
+    "color": "#1E88E5",
+    "files": [
+      "/src/assets/routes/70006_19251B_0.geojson",
+      "/src/assets/routes/70006_19251B_1.geojson"
+    ]
+  },
+  {
+    "id": "19251R",
+    "name": "MLR - MetroLink Red Line",
+    "color": "#43A047",
+    "files": [
+      "/src/assets/routes/70006_19251R_0.geojson",
+      "/src/assets/routes/70006_19251R_1.geojson"
+    ]
+  },
+  {
+    "id": "19592",
+    "name": "1 - Gold",
+    "color": "#E53935",
+    "files": [
+      "/src/assets/routes/70006_19592_0.geojson",
+      "/src/assets/routes/70006_19592_1.geojson"
+    ]
+  },
+  {
+    "id": "19593",
+    "name": "2 - Red",
+    "color": "#FB8C00",
+    "files": [
+      "/src/assets/routes/70006_19593_0.geojson",
+      "/src/assets/routes/70006_19593_1.geojson"
+    ]
+  },
+  {
+    "id": "19594",
+    "name": "4 - Natural Bridge",
+    "color": "#8E24AA",
+    "files": [
+      "/src/assets/routes/70006_19594_0.geojson",
+      "/src/assets/routes/70006_19594_1.geojson"
+    ]
+  },
+  {
+    "id": "19595",
+    "name": "5 - Green",
+    "color": "#00ACC1",
+    "files": [
+      "/src/assets/routes/70006_19595_0.geojson"
+    ]
+  },
+  {
+    "id": "19596",
+    "name": "8 - Shaw-Cherokee",
+    "color": "#FDD835",
+    "files": [
+      "/src/assets/routes/70006_19596_0.geojson",
+      "/src/assets/routes/70006_19596_1.geojson"
+    ]
+  },
+  {
+    "id": "19597",
+    "name": "9 - Oakville",
+    "color": "#6D4C41",
+    "files": [
+      "/src/assets/routes/70006_19597_0.geojson",
+      "/src/assets/routes/70006_19597_1.geojson"
+    ]
+  },
+  {
+    "id": "19598",
+    "name": "10 - Gravois-Lindell",
+    "color": "#3949AB",
+    "files": [
+      "/src/assets/routes/70006_19598_0.geojson",
+      "/src/assets/routes/70006_19598_1.geojson"
+    ]
+  },
+  {
+    "id": "19599",
+    "name": "11 - Chippewa",
+    "color": "#00897B",
+    "files": [
+      "/src/assets/routes/70006_19599_0.geojson",
+      "/src/assets/routes/70006_19599_1.geojson"
+    ]
+  },
+  {
+    "id": "19600",
+    "name": "13 - Union",
+    "color": "#1E88E5",
+    "files": [
+      "/src/assets/routes/70006_19600_0.geojson",
+      "/src/assets/routes/70006_19600_1.geojson"
+    ]
+  },
+  {
+    "id": "19601",
+    "name": "16 - City Limits",
+    "color": "#43A047",
+    "files": [
+      "/src/assets/routes/70006_19601_0.geojson",
+      "/src/assets/routes/70006_19601_1.geojson"
+    ]
+  },
+  {
+    "id": "19602",
+    "name": "18 - Taylor",
+    "color": "#E53935",
+    "files": [
+      "/src/assets/routes/70006_19602_0.geojson",
+      "/src/assets/routes/70006_19602_1.geojson"
+    ]
+  },
+  {
+    "id": "19603",
+    "name": "19 - St Louis Ave",
+    "color": "#FB8C00",
+    "files": [
+      "/src/assets/routes/70006_19603_0.geojson",
+      "/src/assets/routes/70006_19603_1.geojson"
+    ]
+  },
+  {
+    "id": "19604",
+    "name": "21 - Watson Rd",
+    "color": "#8E24AA",
+    "files": [
+      "/src/assets/routes/70006_19604_0.geojson",
+      "/src/assets/routes/70006_19604_1.geojson"
+    ]
+  },
+  {
+    "id": "19605",
+    "name": "30 - Arsenal",
+    "color": "#00ACC1",
+    "files": [
+      "/src/assets/routes/70006_19605_0.geojson",
+      "/src/assets/routes/70006_19605_1.geojson"
+    ]
+  },
+  {
+    "id": "19606",
+    "name": "31 - Chouteau",
+    "color": "#FDD835",
+    "files": [
+      "/src/assets/routes/70006_19606_0.geojson",
+      "/src/assets/routes/70006_19606_1.geojson"
+    ]
+  },
+  {
+    "id": "19607",
+    "name": "32 - Dr ML King",
+    "color": "#6D4C41",
+    "files": [
+      "/src/assets/routes/70006_19607_0.geojson",
+      "/src/assets/routes/70006_19607_1.geojson"
+    ]
+  },
+  {
+    "id": "19608",
+    "name": "33 - Midland",
+    "color": "#3949AB",
+    "files": [
+      "/src/assets/routes/70006_19608_0.geojson",
+      "/src/assets/routes/70006_19608_1.geojson"
+    ]
+  },
+  {
+    "id": "19609",
+    "name": "34 - Earth City",
+    "color": "#00897B",
+    "files": [
+      "/src/assets/routes/70006_19609_0.geojson"
+    ]
+  },
+  {
+    "id": "19610",
+    "name": "35 - Rock Road",
+    "color": "#1E88E5",
+    "files": [
+      "/src/assets/routes/70006_19610_0.geojson",
+      "/src/assets/routes/70006_19610_1.geojson"
+    ]
+  },
+  {
+    "id": "19611",
+    "name": "40 - Broadway-Halls Ferry",
+    "color": "#43A047",
+    "files": [
+      "/src/assets/routes/70006_19611_0.geojson",
+      "/src/assets/routes/70006_19611_1.geojson"
+    ]
+  },
+  {
+    "id": "19612",
+    "name": "41 - Lee",
+    "color": "#E53935",
+    "files": [
+      "/src/assets/routes/70006_19612_0.geojson",
+      "/src/assets/routes/70006_19612_1.geojson"
+    ]
+  },
+  {
+    "id": "19613",
+    "name": "42 - Sarah",
+    "color": "#FB8C00",
+    "files": [
+      "/src/assets/routes/70006_19613_0.geojson",
+      "/src/assets/routes/70006_19613_1.geojson"
+    ]
+  },
+  {
+    "id": "19614",
+    "name": "49 - Lindbergh",
+    "color": "#8E24AA",
+    "files": [
+      "/src/assets/routes/70006_19614_0.geojson",
+      "/src/assets/routes/70006_19614_1.geojson"
+    ]
+  },
+  {
+    "id": "19615",
+    "name": "56 - Kirkwood-Webster",
+    "color": "#00ACC1",
+    "files": [
+      "/src/assets/routes/70006_19615_0.geojson",
+      "/src/assets/routes/70006_19615_1.geojson"
+    ]
+  },
+  {
+    "id": "19616",
+    "name": "57 - Manchester",
+    "color": "#FDD835",
+    "files": [
+      "/src/assets/routes/70006_19616_0.geojson",
+      "/src/assets/routes/70006_19616_1.geojson"
+    ]
+  },
+  {
+    "id": "19617",
+    "name": "58 - Chesterfield Valley",
+    "color": "#6D4C41",
+    "files": [
+      "/src/assets/routes/70006_19617_0.geojson",
+      "/src/assets/routes/70006_19617_1.geojson"
+    ]
+  },
+  {
+    "id": "19618",
+    "name": "59 - Oakland",
+    "color": "#3949AB",
+    "files": [
+      "/src/assets/routes/70006_19618_0.geojson",
+      "/src/assets/routes/70006_19618_1.geojson"
+    ]
+  },
+  {
+    "id": "19619",
+    "name": "60 - Shepley-Lilac",
+    "color": "#00897B",
+    "files": [
+      "/src/assets/routes/70006_19619_0.geojson",
+      "/src/assets/routes/70006_19619_1.geojson"
+    ]
+  },
+  {
+    "id": "19620",
+    "name": "61 - Chambers Rd",
+    "color": "#1E88E5",
+    "files": [
+      "/src/assets/routes/70006_19620_0.geojson",
+      "/src/assets/routes/70006_19620_1.geojson"
+    ]
+  },
+  {
+    "id": "19621",
+    "name": "64 - Lucas-Hunt",
+    "color": "#43A047",
+    "files": [
+      "/src/assets/routes/70006_19621_0.geojson",
+      "/src/assets/routes/70006_19621_1.geojson"
+    ]
+  },
+  {
+    "id": "19622",
+    "name": "70 - Grand",
+    "color": "#E53935",
+    "files": [
+      "/src/assets/routes/70006_19622_0.geojson",
+      "/src/assets/routes/70006_19622_1.geojson"
+    ]
+  },
+  {
+    "id": "19623",
+    "name": "73 - Carondelet",
+    "color": "#FB8C00",
+    "files": [
+      "/src/assets/routes/70006_19623_0.geojson",
+      "/src/assets/routes/70006_19623_1.geojson"
+    ]
+  },
+  {
+    "id": "19624",
+    "name": "74 - Florissant",
+    "color": "#8E24AA",
+    "files": [
+      "/src/assets/routes/70006_19624_0.geojson",
+      "/src/assets/routes/70006_19624_1.geojson"
+    ]
+  },
+  {
+    "id": "19625",
+    "name": "76 - N Hanley Shuttle",
+    "color": "#00ACC1",
+    "files": [
+      "/src/assets/routes/70006_19625_0.geojson",
+      "/src/assets/routes/70006_19625_1.geojson"
+    ]
+  },
+  {
+    "id": "19626",
+    "name": "77 - Village Square",
+    "color": "#FDD835",
+    "files": [
+      "/src/assets/routes/70006_19626_0.geojson",
+      "/src/assets/routes/70006_19626_1.geojson"
+    ]
+  },
+  {
+    "id": "19627",
+    "name": "78 - Bellefontaine",
+    "color": "#6D4C41",
+    "files": [
+      "/src/assets/routes/70006_19627_0.geojson",
+      "/src/assets/routes/70006_19627_1.geojson"
+    ]
+  },
+  {
+    "id": "19628",
+    "name": "79 - Ferguson-Clayton",
+    "color": "#3949AB",
+    "files": [
+      "/src/assets/routes/70006_19628_0.geojson",
+      "/src/assets/routes/70006_19628_1.geojson"
+    ]
+  },
+  {
+    "id": "19629",
+    "name": "90 - Hampton",
+    "color": "#00897B",
+    "files": [
+      "/src/assets/routes/70006_19629_0.geojson",
+      "/src/assets/routes/70006_19629_1.geojson"
+    ]
+  },
+  {
+    "id": "19630",
+    "name": "91 - Olive",
+    "color": "#1E88E5",
+    "files": [
+      "/src/assets/routes/70006_19630_0.geojson",
+      "/src/assets/routes/70006_19630_1.geojson"
+    ]
+  },
+  {
+    "id": "19631",
+    "name": "94 - Page",
+    "color": "#43A047",
+    "files": [
+      "/src/assets/routes/70006_19631_0.geojson",
+      "/src/assets/routes/70006_19631_1.geojson"
+    ]
+  },
+  {
+    "id": "19632",
+    "name": "95 - Kingshighway",
+    "color": "#E53935",
+    "files": [
+      "/src/assets/routes/70006_19632_0.geojson",
+      "/src/assets/routes/70006_19632_1.geojson"
+    ]
+  },
+  {
+    "id": "19633",
+    "name": "97 - Delmar",
+    "color": "#FB8C00",
+    "files": [
+      "/src/assets/routes/70006_19633_0.geojson",
+      "/src/assets/routes/70006_19633_1.geojson"
+    ]
+  },
+  {
+    "id": "19634",
+    "name": "98 - Ballas-North Hanley",
+    "color": "#8E24AA",
+    "files": [
+      "/src/assets/routes/70006_19634_0.geojson",
+      "/src/assets/routes/70006_19634_1.geojson"
+    ]
+  },
+  {
+    "id": "19635",
+    "name": "100 - Hazelwood",
+    "color": "#00ACC1",
+    "files": [
+      "/src/assets/routes/70006_19635_0.geojson",
+      "/src/assets/routes/70006_19635_1.geojson"
+    ]
+  },
+  {
+    "id": "19636",
+    "name": "1 - Main Street - State Street",
+    "color": "#FDD835",
+    "files": [
+      "/src/assets/routes/70006_19636_0.geojson",
+      "/src/assets/routes/70006_19636_1.geojson"
+    ]
+  },
+  {
+    "id": "19637",
+    "name": "2 - Cahokia Heights",
+    "color": "#6D4C41",
+    "files": [
+      "/src/assets/routes/70006_19637_0.geojson",
+      "/src/assets/routes/70006_19637_1.geojson"
+    ]
+  },
+  {
+    "id": "19638",
+    "name": "3 - Sauget - Water Street",
+    "color": "#3949AB",
+    "files": [
+      "/src/assets/routes/70006_19638_0.geojson",
+      "/src/assets/routes/70006_19638_1.geojson"
+    ]
+  },
+  {
+    "id": "19639",
+    "name": "4 - 19th & Central",
+    "color": "#00897B",
+    "files": [
+      "/src/assets/routes/70006_19639_0.geojson"
+    ]
+  },
+  {
+    "id": "19640",
+    "name": "5 - Missouri Ave - ML King",
+    "color": "#1E88E5",
+    "files": [
+      "/src/assets/routes/70006_19640_0.geojson"
+    ]
+  },
+  {
+    "id": "19641",
+    "name": "6 - Rosemont",
+    "color": "#43A047",
+    "files": [
+      "/src/assets/routes/70006_19641_0.geojson",
+      "/src/assets/routes/70006_19641_1.geojson"
+    ]
+  },
+  {
+    "id": "19642",
+    "name": "8 - Alta Sita",
+    "color": "#E53935",
+    "files": [
+      "/src/assets/routes/70006_19642_0.geojson",
+      "/src/assets/routes/70006_19642_1.geojson"
+    ]
+  },
+  {
+    "id": "19643",
+    "name": "9 - Washington Park",
+    "color": "#FB8C00",
+    "files": [
+      "/src/assets/routes/70006_19643_0.geojson",
+      "/src/assets/routes/70006_19643_1.geojson"
+    ]
+  },
+  {
+    "id": "19644",
+    "name": "12 - OFallon-Fairview Heights",
+    "color": "#8E24AA",
+    "files": [
+      "/src/assets/routes/70006_19644_0.geojson",
+      "/src/assets/routes/70006_19644_1.geojson"
+    ]
+  },
+  {
+    "id": "19645",
+    "name": "13 - Caseyville",
+    "color": "#00ACC1",
+    "files": [
+      "/src/assets/routes/70006_19645_0.geojson",
+      "/src/assets/routes/70006_19645_1.geojson"
+    ]
+  },
+  {
+    "id": "19646",
+    "name": "14 - Memorial Hosp-Westfield Plaza",
+    "color": "#FDD835",
+    "files": [
+      "/src/assets/routes/70006_19646_0.geojson",
+      "/src/assets/routes/70006_19646_1.geojson"
+    ]
+  },
+  {
+    "id": "19647",
+    "name": "15 - Belleville-O'Fallon",
+    "color": "#6D4C41",
+    "files": [
+      "/src/assets/routes/70006_19647_0.geojson",
+      "/src/assets/routes/70006_19647_1.geojson"
+    ]
+  },
+  {
+    "id": "19648",
+    "name": "16 - St. Clair Square",
+    "color": "#3949AB",
+    "files": [
+      "/src/assets/routes/70006_19648_0.geojson",
+      "/src/assets/routes/70006_19648_1.geojson"
+    ]
+  },
+  {
+    "id": "19649",
+    "name": "23 - Belleville-College",
+    "color": "#00897B",
+    "files": [
+      "/src/assets/routes/70006_19649_0.geojson",
+      "/src/assets/routes/70006_19649_1.geojson"
+    ]
+  },
+  {
+    "id": "19651B",
+    "name": "MLB - MetroLink Blue Line",
+    "color": "#1E88E5",
+    "files": [
+      "/src/assets/routes/70006_19651B_0.geojson",
+      "/src/assets/routes/70006_19651B_1.geojson"
+    ]
+  },
+  {
+    "id": "19651R",
+    "name": "MLR - MetroLink Red Line",
+    "color": "#43A047",
+    "files": [
+      "/src/assets/routes/70006_19651R_0.geojson",
+      "/src/assets/routes/70006_19651R_1.geojson"
+    ]
+  }
+];
+
+export default routesMetadata;
